889fd0caa3e76ad4b6b414e8bf415440056e537a
Jason <jasonlucas443@gmail.com>
2022-06-02 00:03:14 -0300

    refactor: pratica sql 2

--- exec2/answers.sql
+++ exec2/answers.sql
@@ -1,7 +1,7 @@
 select count(experiences."endDate") as currentExperiences from experiences; -- q1
 
-select ("users"."id", count(edc.status)) from educations edc join users on edc."userId" = "users"."id" where edc."status" = 'finished' group by "users"."id"; -- q2
+select users.id, count(edc.status) as educations from educations edc join users on edc."userId" = "users"."id" where edc."status" = 'finished' group by "users"."id"; -- q2
 
-select (users."name", count(testimonials."message")) from testimonials join users on testimonials."writerId"="users"."id" where "users"."id"=435 group by "users"."name"; -- q3
+select users.name as writer, count(testimonials."message") as testimonialCount from testimonials join users on testimonials."writerId"="users"."id" where "users"."id"=435 group by "users"."name"; -- q3
 
-select (jobs."salary", roles.name) from jobs join roles on jobs."roleId"="roles"."id" where jobs."active"=true order by jobs."salary" desc; -- q4
+select jobs.salary as maximumSalary, roles.name as role from jobs join roles on jobs."roleId"="roles"."id" where jobs."active"=true order by maximumSalary asc; -- q4
